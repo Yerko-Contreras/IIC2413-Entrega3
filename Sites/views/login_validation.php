b1580f87b0c0885e707e76cce676c16760aabfee
Martin Rosselot <martin.rosselot@uc.cl>
2022-06-15 20:10:52 -0400

    aa

--- Sites/views/login_validation.php
+++ Sites/views/login_validation.php
@@ -30,8 +30,11 @@
             $_SESSION['username'] = $data[0];
             $_SESSION['password'] = $data[1];
             $_SESSION['tipo'] = $data[2];
-            
+            $msg = "Sesión iniciada correctamente";
+            header("Location: ../index.php?msg=$msg");
         } else {
+            $msg = 'Login Invalido';
+            header("Location: ../views/login.php?msg=$msg");
         };
     };
 ?>
