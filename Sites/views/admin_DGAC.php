81e5986546c6e6c80ca129880d238692d0e36d24
Yerko Contreras <65320230+Yerko-Contreras@users.noreply.github.com>
2022-06-18 14:46:04 -0400

    Login

--- Sites/views/admin_DGAC.php
+++ Sites/views/admin_DGAC.php
@@ -17,20 +17,28 @@
     <tr>
       <th>Aerolinea</th>
       <th>Porcentaje de Vuelos Aprobados (%)</th>
+      <th>Porcentaje de Vuelos Aprobados (%)</th>
+       <th>Porcentaje de Vuelos Aprobados (%)</th>
     </tr>
   
       <?php
+          echo "<tr>";
+          echo "<td>" . $row['id'] . "</td>";
+          echo "<td>" . $row['name'] . "</td>";
+          echo "<td>" . $row['price'] . "</td>";
+          echo "</tr>";
         // echo $resultado;
         foreach ($resultado as $p) {
-          echo "<tr><td>$p[0]</td><td>$p[1]</td></tr>";
+          echo "<tr>";
+          echo "<td>$p[0]</td><td>$p[1]</td>";
           
           if(isset($_POST['button1'])) {
             echo "This is Button1 that is selected";
           }
-
           if(isset($_POST['button2'])) {
               echo "This is Button2 that is selected";
           }
+          echo "</tr>";
       }
       ?>
       
@@ -43,5 +51,5 @@
       <input type="submit" name="button2"
               value="Button2"/>
   </form>
-  
+
 <?php include('../templates/footer.html'); ?>
