228ab7a72e3f5c3f24224a55d45a3637ee630c69
gonzaloi21 <gonzaloi@uc.cl>
2022-06-18 16:55:02 -0400

    hola

---
+++ Sites/admi_DGAC.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+	$msg = $_GET['msg']
+?>
+<?php
+    include("../templates/header.html");
+?>
+
+<body> 
+    <br>
+    <h1 class="title is-3 has-text-centered">Administrador DGAC</h1>
+    <br>
+	<br>
