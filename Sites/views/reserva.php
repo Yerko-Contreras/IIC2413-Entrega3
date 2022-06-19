b28a56e721d784a6a9687093c573cd0609e5048c
Martin Rosselot <martin.rosselot@uc.cl>
2022-06-19 15:02:57 -0400

    Reserva

--- Sites/views/reserva.php
+++ Sites/views/reserva.php
@@ -3,9 +3,21 @@
     $codigo_vuelo = $_POST['codigo_vuelo'];
     require("../config/conection.php");
     if (isset($_POST['pasaporte_1']) || isset($_POST['pasaporte_2']) || isset($_POST['pasaporte_3'])){
-        $pasaporte_1 = $_POST['pasaporte_1'];
-        $pasaporte_1 = $_POST['pasaporte_2'];
-        $pasaporte_1 = $_POST['pasaporte_3'];
+        if (isset($_POST['pasaporte_1'])){
+            $pasaporte_1 = $_POST['pasaporte_1'];
+        } else {
+            $pasaporte_1 = '';
+        };
+        if (isset($_POST['pasaporte_2'])){
+            $pasaporte_2 = $_POST['pasaporte_2'];
+        } else {
+            $pasaporte_2 = '';
+        };
+        if (isset($_POST['pasaporte_3'])){
+            $pasaporte_3 = $_POST['pasaporte_3'];
+        } else {
+            $pasaporte_3 = '';
+        };
         $query = "SELECT * FROM crear_reserva('$codigo_vuelo', '$pasaporte_1', '$pasaporte_2', '$pasaporte_3', '$pasaporte');";
         $result = $db -> prepare($query);
         $result -> execute();
