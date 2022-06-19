0e50be8f079121d04608d35bc0207eddd72b302f
Yerko Contreras <65320230+Yerko-Contreras@users.noreply.github.com>
2022-06-19 13:26:37 -0400

    Commit

--- Sites/views/compania_aerea.php
+++ Sites/views/compania_aerea.php
@@ -10,19 +10,19 @@
   $username = $_GET['username'];
 
   #Se construye la consulta como un string
-  $query = "SELECT * FROM vuelo where estado = 'aceptado' AND codigo_compania = '$username'";
+  $query = "SELECT vuelo.vuelo_id, vuelo.codigo_vuelo, aerodromo1.nombre, aerodromo2.nombre, vuelo.fecha_salida, vuelo.fecha_llegada FROM vuelo, aerodromo as aerodromo1, aerodromo as aerodromo2 where vuelo.estado = 'aceptado' AND vuelo.codigo_compania = '$username' AND vuelo.aerodromo_salida_id = aerodromo1.aerodromo_id AND vuelo.aerodromo_llegada_id = aerodromo2.aerodromo_id";
 
   $result = $db -> prepare($query);
 	$result -> execute();
 	$resultado = $result -> fetchAll();
-  $query1 = "SELECT * FROM vuelo where estado = 'rechazado' AND codigo_compania = '$username'";
+  $query1 = "SELECT vuelo.vuelo_id, vuelo.codigo_vuelo, aerodromo1.nombre, aerodromo2.nombre, vuelo.fecha_salida, vuelo.fecha_llegada FROM vuelo, aerodromo as aerodromo1, aerodromo as aerodromo2 where vuelo.estado = 'rechazado' AND vuelo.codigo_compania = '$username' AND vuelo.aerodromo_salida_id = aerodromo1.aerodromo_id AND vuelo.aerodromo_llegada_id = aerodromo2.aerodromo_id";
 
   $result1 = $db -> prepare($query1);
 	$result1 -> execute();
 	$resultado1 = $result1 -> fetchAll();
   ?>
 
-<h3 align="center">Vuelos Aprobados</h3>
+<h3 class="title is-2 has-text-weight-bold has-text-centered">Vuelos Aprobados</h3>
   <table class = "table is-bordered is-striped is-narrow is-hoverable is-fullwidth has-background-info-light" align="center" >
     <tr>
       <th>ID</th>
@@ -37,8 +37,7 @@
       <?php
         // echo $resultado;
         foreach ($resultado as $p) {
-          echo "$username";
-          echo "<tr><td>$p[0]</td><td>$p[4]</td><td>$p[1]</td><td>$p[2]</td><td>$p[7]</td><td>$p[8]</td></tr>";
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td></tr>";
 
       }
       ?>
@@ -48,7 +47,7 @@
   </table>
 <br>
 <br>
-<h3 align="center">Vuelos Rechazados</h3>
+<h3 class="title is-2 has-text-weight-bold has-text-centered">Vuelos Rechazados</h3>
   <table class = "table is-bordered is-striped is-narrow is-hoverable is-fullwidth has-background-info-light" align="center" >
     <tr>
       <th>ID</th>
