e56c07d92599b88e47ce09cb6e69ca5983fab47a
JOSEFA FERNANDEZ <josefa.fernandez@uc.cl>
2022-06-20 19:21:46 -0400

    col5

--- Sites/views/admin_DGAC.php
+++ Sites/views/admin_DGAC.php
@@ -16,12 +16,22 @@
   <table align="center">
     <tr>
       <th>Vuelo ID</th>
+      <th>Aerodromo Salida ID</th>
+      <th>Aerodromo Llegada ID</th>
+      <th>Ruta ID</th>
       <th>Codigo Vuelo</th>
+      <th>Codigo Aeronave</th>
+      <th>Codigo Compañia</th>
       <th>Fecha Salida</th>
       <th>Fecha Llegada</th>
       <th>Velocidad</th>
       <th>Altitud</th>
       <th>Estado</th>
+      <th>Nombre Aeronave</th>
+      <th>Modelo</th>
+      <th>Peso</th>
+      <th>Valor</th>
+      <th>Nombre Compañia</th>
       <th>Boton 1</th>
       <th>Boton 2</th>
     </tr>
@@ -30,7 +40,7 @@
         // echo $resultado;
         foreach ($resultado as $p) {
           echo "<tr>";
-          echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td>";
+          echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td><td>$p[12]</td><td>$p[13]</td><td>$p[14]</td><td>$p[15]</td>";
           echo "<td><input type='button' name='Button1' value='ACEPTAR'></td>";
 
           echo "<td><input type='button' name='buysell' value='RECHAZAR'></td>";
