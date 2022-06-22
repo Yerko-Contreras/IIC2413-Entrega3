46d43c33b5cb9a5d6b46728c1fdc1a3b13325ba6
JOSEFA FERNANDEZ <josefa.fernandez@uc.cl>
2022-06-22 10:12:24 -0400

    fecha

--- Sites/views/admin_DGAC.php
+++ Sites/views/admin_DGAC.php
@@ -5,7 +5,52 @@
 <h3 align="center"><b>ADMIN</b></h3>
 <br>
 
-<h3 align="center">Filtrar por fecha las propuestas de vuelo pendiente</h3>
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conection.php");
+
+  #Se construye la consulta como un string
+  $query = "SELECT * FROM vuelo";
+
+  $result = $db -> prepare($query);
+	$result -> execute();
+	$resultado = $result -> fetchAll();
+  ?>
+<h3 align="center">ADMIN</h3>
+<br>
+        <table align="center">
+    <tr>
+      <th>Vuelo ID</th>
+      <th>Aerodromo Salida ID</th>
+      <th>Aerodromo Llegada ID</th>
+      <th>Ruta ID</th>
+      <th>Codigo Vuelo</th>
+      <th>Codigo Aeronave</th>
+      <th>Codigo Compañia</th>
+      <th>Fecha Salida</th>
+      <th>Fecha Llegada</th>
+      <th>Velocidad</th>
+      <th> Altitud</th>
+      <th> Estado</th>
+      <th> Aceptar</th>
+      <th> Rechazar</th>
+    </tr>
+  
+      <?php
+        // echo $resultado;
+        foreach ($resultado as $p) {
+          echo "<tr>";
+          echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td>";
+          echo "</tr>";
+      }
+      ?>
+ 
+  </table>
+
+
+
+
+<h3 align="center">Filtrar por fecha las propuestas de Vuelos Pendientes</h3>
 <br>
   <?php
   #Primero obtenemos todos los tipos de pokemones
