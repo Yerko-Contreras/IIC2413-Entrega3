64336e0a804418cc9e269fb27cf3a16de8f10503
JOSEFA FERNANDEZ <josefa.fernandez@uc.cl>
2022-06-22 09:30:32 -0400

    fechas

--- Sites/views/admin_DGAC.php
+++ Sites/views/admin_DGAC.php
@@ -1,50 +1,85 @@
 <?php include('../templates/header.html');   ?>
 
 <body>
-<?php
-  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conection.php");
-
-  #Se construye la consulta como un string
-  $query = "SELECT * FROM vuelo";
-
-  $result = $db -> prepare($query);
-	$result -> execute();
-	$resultado = $result -> fetchAll();
-  ?>
 <h3 align="center">ADMIN</h3>
-  <table align="center">
-    <tr>
-      <th>Vuelo ID</th>
-      <th>Aerodromo Salida ID</th>
-      <th>Aerodromo Llegada ID</th>
-      <th>Ruta ID</th>
-      <th>Codigo Vuelo</th>
-      <th>Codigo Aeronave</th>
-      <th>Codigo Compañia</th>
-      <th>Fecha Salida</th>
-      <th>Fecha Llegada</th>
-      <th>Velocidad</th>
-      <th> Altitud</th>
-      <th> Estado</th>
-      <th> Aceptar</th>
-      <th> Rechazar</th>
-    </tr>
-  
+<br>
+
+<h3 align="center">Filtrar por fecha las propuestas de vuelo pendiente</h3>
+  <?php
+  #Primero obtenemos todos los tipos de pokemones
+  require("../config/conection.php");
+  $result = $db -> prepare("SELECT DISTINCT fecha_salida, DISTINCT fecha_llegada FROM vuelo;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+
+  <form align="center" action=".../views/admin_DGAC.php" method="post">
+    Seleccinar Fechas:
+    Fecha Inicio/Salida:
+    <select name="inicio">
       <?php
-        // echo $resultado;
-        foreach ($resultado as $p) {
-          echo "<tr>";
-          echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td>";
-          if ($p[11] == "pendiente") {
-            echo "<td><input type='button' name='Button1' value='Aceptar'></td>";
-            echo "<td><input type='button' name='buysell' value='Rechazar'></td>";}
-          echo "</tr>";
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
       }
       ?>
-      
-  </table>
+    </select>
+    <br/>
+    Fecha Final/Llegada:
+    <select name="final">
+      <?php
+      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[1]>$d[1]</option>";
+      }
+      ?>
+    </select>
+    <br/><br/>
+    <input type="submit" value="Buscar fechas">
+  </form>
 
+  <?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+  $inicio = $_POST["inicio"];
+  $final = $_POST["final"];
+
+ 	$query2 = "SELECT * FROM vuelo WHERE CAST(fecha_salida AS date) >= CAST('$inicial' AS date) AND CAST(fecha_salida AS date) <= CAST('$final' AS date) OR CAST(fecha_llegada AS date) >= CAST('$inicial' AS date) AND CAST(fecha_llegada AS date) <= CAST('$final' AS date)";
+	$result = $db -> prepare($query2);
+	$result -> execute();
+	$vuelos = $result -> fetchAll();
+  ?>
+  <h3 align="center">Vuelos Pendientes</h3>
+	<table align="center">
+  <tr>
+    <th>Vuelo ID</th>
+    <th>Aerodromo Salida ID</th>
+    <th>Aerodromo Llegada ID</th>
+    <th>Ruta ID</th>
+    <th>Codigo Vuelo</th>
+    <th>Codigo Aeronave</th>
+    <th>Codigo Compañia</th>
+    <th>Fecha Salida</th>
+    <th>Fecha Llegada</th>
+    <th>Velocidad</th>
+    <th> Altitud</th>
+    <th> Estado</th>
+    <th> Aceptar</th>
+    <th> Rechazar</th>
+  </tr>
+    <?php
+      // echo $resultado;
+      foreach ($resultado as $p) {
+        echo "<tr>";
+        echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td>";
+        if ($p[11] == "pendiente") {
+          echo "<td><input type='button' name='Button1' value='Aceptar'></td>";
+          echo "<td><input type='button' name='Button2' value='Rechazar'></td>";}
+        echo "</tr>";
+    }
+    ?>
+</table>
 
 
 <?php include('../templates/footer.html'); ?>
