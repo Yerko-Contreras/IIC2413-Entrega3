0e69a0e0a458c925962af64327ac820ddcb7e090
Yerko Contreras <65320230+Yerko-Contreras@users.noreply.github.com>
2022-06-25 04:20:14 -0400

    Botones

--- Sites/views/admin_DGAC.php
+++ Sites/views/admin_DGAC.php
@@ -115,7 +115,7 @@
       // echo $resultado;
       foreach ($resultado as $p) {
         echo "<tr>";
-        echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td>";
+        echo "<td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[10]</td><td>$p[11]</td>";
         if ($p[11] == "pendiente") {
           echo '<input type="submit" name="btnSubmit" value="Save Changes">';
           echo '<input type="submit" name="btnDelete" value="Delete">';}
