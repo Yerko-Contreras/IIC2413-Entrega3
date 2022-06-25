d7307157e9b8c3b52828448eee0a9b02ce21f8eb
JOSEFA FERNANDEZ <josefa.fernandez@uc.cl>
2022-06-25 18:56:08 -0400

    tabla

--- Sites/views/admin_DGAC.php
+++ Sites/views/admin_DGAC.php
@@ -14,7 +14,7 @@
   require("../config/conection.php");
 
   #Se construye la consulta como un string
-  $query = "SELECT propuestas.informacion_id, propuestas.propuesta_vuelo_id, propuestas.codigo_compania, informacion_de_vuelo.codigo, informacion_de_vuelo.codigo_aeronave,informacion_de_vuelo.aerodromo_id_sal, informacion_de_vuelo.aerodromo_id_lle, propuestas.fecha_envio_propuesta, informacion_de_vuelo.fecha_salida, informacion_de_vuelo.fecha_llegada, fpl.pasaporte, fpl.tipo_vuelo, fpl.max_pasajeros, fpl.velocidad, fpl.altitud, fpl.realizado, informacion_de_vuelo.estado FROM informacion_de_vuelo, fpl, propuestas WHERE fpl.informacion_id = propuestas.informacion_id AND propuestas.informacion_id = informacion_de_vuelo.informacion_id;";
+  $query = "SELECT propuestas.propuesta_vuelo_id, propuestas.codigo_compania, informacion_de_vuelo.codigo, informacion_de_vuelo.codigo_aeronave,informacion_de_vuelo.aerodromo_id_sal, informacion_de_vuelo.aerodromo_id_lle, propuestas.fecha_envio_propuesta, informacion_de_vuelo.fecha_salida, informacion_de_vuelo.fecha_llegada, fpl.tipo_vuelo, fpl.max_pasajeros, fpl.velocidad, fpl.altitud, fpl.realizado, informacion_de_vuelo.estado FROM informacion_de_vuelo, fpl, propuestas WHERE fpl.informacion_id = propuestas.informacion_id AND propuestas.informacion_id = informacion_de_vuelo.informacion_id;";
 
   $result = $db2 -> prepare($query);
 	$result -> execute();
@@ -27,7 +27,6 @@
 <br>
 <table class='table is-bordered is-striped is-narrow is-hoverable is-fullwidth has-background-info-light' align="center">
   <tr>
-    <th>Informacion ID</th>
     <th>Propuesta vuelo ID</th>
     <th>Codigo de compañia</th>
     <th>Codigo de vuelo</th>
@@ -37,7 +36,6 @@
     <th>Fecha envio de propuesta</th>
     <th>Fecha de salida</th>
     <th>Fecha de llegada</th>
-    <th>Pasaporte</th>
     <th>Tipo de vuelo</th>
     <th>Maximo de pasajeros</th>
     <th>Velocidad</th>
@@ -48,7 +46,7 @@
     <?php
       // echo $resultado;
       foreach ($resultado as $p) {
-        echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td><td>$p[12]</td><td>$p[13]</td><td>$p[14]</td><td>$p[15]</td><td>$p[16]</td></tr>";
+        echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td><td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td><td>$p[9]</td><td>$p[10]</td><td>$p[11]</td><td>$p[12]</td><td>$p[13]</td><td>$p[14]</td></tr>";
     }
     ?>
 </table>
