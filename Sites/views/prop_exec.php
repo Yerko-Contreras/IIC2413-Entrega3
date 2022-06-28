59b2dfc1d549250192ef408e70f976f764cb3535
JOSEFA FERNANDEZ <josefa.fernandez@uc.cl>
2022-06-28 15:27:47 -0400

    e

--- Sites/views/prop_exec.php
+++ Sites/views/prop_exec.php
@@ -3,16 +3,16 @@
         echo("You clicked button 1!");
         //and then execute a sql query here
         require("../config/conection.php");
-            $username = $_SESSION['username'];
+            session_start();
             $codigo = $_POST['codigo'];
                
             $query1 = "UPDATE vuelo SET vuelo.estado='aceptado' WHERE vuelo.codigo_vuelo ='$codigo';";
             $result1 = $db -> prepare($query1);
             $result1 -> execute();
 
-            $query2 = "UPDATE informacion_de_vuelo SET informacion_de_vuelo.estado='aceptado' WHERE informacion_de_vuelo.codigo_vuelo ='$p[2]';";
+            $query2 = "UPDATE informacion_de_vuelo SET informacion_de_vuelo.estado='aceptado' WHERE informacion_de_vuelo.codigo_vuelo ='$codigo';";
             $result2 = $db2 -> prepare($query2);
-            $result12 -> execute();
+            $result2 -> execute();
     }
     else {
     echo" dhur";
@@ -24,18 +24,22 @@
         echo("You clicked button 2!");
         //and then execute a sql query here
         require("../config/conection.php");
-            $username = $_SESSION['username'];
+            session_start();
             $codigo = $_POST['codigo'];
                
             $query1 = "UPDATE vuelo SET vuelo.estado='rechazado' WHERE vuelo.codigo_vuelo ='$codigo';";
             $result1 = $db -> prepare($query1);
             $result1 -> execute();
 
-            $query2 = "UPDATE informacion_de_vuelo SET informacion_de_vuelo.estado='rechazado' WHERE informacion_de_vuelo.codigo_vuelo ='$p[2]';";
+            $query2 = "UPDATE informacion_de_vuelo SET informacion_de_vuelo.estado='rechazado' WHERE informacion_de_vuelo.codigo_vuelo ='$codigo';";
             $result2 = $db2 -> prepare($query2);
-            $result12 -> execute();
+            $result2 -> execute();
     }
     else {
     echo" dhur";
     }
+?>
+
+<?php
+require('admin_DGAC.php')
 ?>
